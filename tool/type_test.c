9463636277a311e2468bb9461e0650026bc75e86
along <along@126.com>
2012-11-26 23:25:48 +0800

    :q确保此代码可以在64w位机器上编译通过

--- tool/type_test.c
+++ tool/type_test.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(int argc,char**argv)
 {
 	printf("test system type length:\n");
-	printf("sizeof(char)=%u\n",sizeof(char));
-	printf("sizeof(short)=%u\n",sizeof(short));
-	printf("sizeof(int)=%u\n",sizeof(int));
-	printf("sizeof(long)=%u\n",sizeof(long));
-	printf("sizeof(long long)=%u\n",sizeof(long long));
-	printf("sizeof(float)=%u\n",sizeof(float));
-	printf("sizeof(double)=%u\n",sizeof(double));
-	printf("sizeof(long double)=%u\n",sizeof(long double));
+	printf("(uint32_t)sizeof(char)=%u\n",(uint32_t)sizeof(char));
+	printf("(uint32_t)sizeof(short)=%u\n",(uint32_t)sizeof(short));
+	printf("(uint32_t)sizeof(int)=%u\n",(uint32_t)sizeof(int));
+	printf("(uint32_t)sizeof(long)=%u\n",(uint32_t)sizeof(long));
+	printf("(uint32_t)sizeof(long long)=%u\n",(uint32_t)sizeof(long long));
+	printf("(uint32_t)sizeof(float)=%u\n",(uint32_t)sizeof(float));
+	printf("(uint32_t)sizeof(double)=%u\n",(uint32_t)sizeof(double));
+	printf("(uint32_t)sizeof(long double)=%u\n",(uint32_t)sizeof(long double));
 
 	return 0;
 }
