da78039ae6a0a1907d949ff9a0c68432bb694818
anlaneg <anlaneg@a.com>
2012-11-26 14:46:06 +0800

    有那么一天,不知天高地厚的我,定义了一个自已的语言,这里语言的样例(现在来看这个模型可以再完美一些,注要有以下特性:1.认为字符串即代码,代码即为字符串;2.认为在修改一个历史代码时,可以不修改代码本身,而限定在代码某一处应用时,需要变更为新的方式;3.引入了事件机制(不是C#中的事件),这种事件机制为了提升调试的灵活性,属于代码级支持;4.这是个动态语言)

---
+++ my_lan.c
@@ -0,0 +1,116 @@
+packet cn.along.jd;
+
+import c.xx;
+import java.xx.xx;
+import ruby.xx;
+
+class B modify
+{
+	xxx;
+	old();
+	xxx;
+	old();
+	xxx;
+}
+
+class A extends class B
+{
+	public  A() {
+		super("along");
+		s = new Array();
+	}
+
+	var s;
+	var f;
+	var t;
+
+	pulbic sayHello()
+	{
+		system.out.println("hello world");
+	}
+
+	public sayHello(exe)throws testException;
+	{
+		exe.execute();
+
+		var s = 3;
+		3.abcdef();
+	}
+
+	public sayHello(function p (){xxxxxx})
+	{
+		for(var i = 0 ; i < s.length;++i)
+		{
+			p(s[i]);
+		}
+	}
+
+	public sayHello(class P(testxx) {xxx,xxx,xxx})
+	{
+		for(var i = 0 ; i < s.length;++i)
+		{
+			P.xx(s[i]);
+			p.xx(s[i]);
+			p.xx().xx();
+		}
+	}
+
+	public do_it(function p(){})
+	{
+		"xxx".execute("xxx");
+	}
+
+	//事件有哪几种
+	//function entry,leave,on_exception,on_return,
+        //function onxxx_modify_before,onxxx_modify_after
+        //
+	public event xx_before()
+	{
+	}
+
+	public event xx_edit()
+	{
+	}
+
+	public event xx_edit()
+	{
+	}
+
+	public event xx_on_entern()
+	{
+	}
+
+	public event xx_on_leveaf()
+	{
+	}
+}
+
+A.functions=public function()
+{
+	xxxxxx;
+}
+
+A.functions=public function()
+{
+}
+
+A.functions=public function()
+{
+}
+
+A.methods=public function()
+{
+}
+
+A.members=var i;
+
+A.
+//1.   2.   3.   4.   5
+//1 1.1 1.2  1.3
+//2 2.1 2.2  2.3
+//3 3.1 3.2  3.3
+
+
+//1.代码即为字符串,字符串即为代码
+//2.一开始我只写了一个大纲,后来我把这些大纲变成了一本书
+//3.
