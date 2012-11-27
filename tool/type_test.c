8118791ce224509968e519421f39ca3626bfb346
anlaneg <anlaneg@a.com>
2012-11-27 20:24:54 +0800

    给出一个清晰的输出

--- tool/type_test.c
+++ tool/type_test.c
@@ -4,14 +4,14 @@
 int main(int argc,char**argv)
 {
 	printf("test system type length:\n");
-	printf("(uint32_t)sizeof(char)=%u\n",(uint32_t)sizeof(char));
-	printf("(uint32_t)sizeof(short)=%u\n",(uint32_t)sizeof(short));
-	printf("(uint32_t)sizeof(int)=%u\n",(uint32_t)sizeof(int));
-	printf("(uint32_t)sizeof(long)=%u\n",(uint32_t)sizeof(long));
-	printf("(uint32_t)sizeof(long long)=%u\n",(uint32_t)sizeof(long long));
-	printf("(uint32_t)sizeof(float)=%u\n",(uint32_t)sizeof(float));
-	printf("(uint32_t)sizeof(double)=%u\n",(uint32_t)sizeof(double));
-	printf("(uint32_t)sizeof(long double)=%u\n",(uint32_t)sizeof(long double));
+	printf("sizeof(char)=%u\n",(uint32_t)sizeof(char));
+	printf("sizeof(short)=%u\n",(uint32_t)sizeof(short));
+	printf("sizeof(int)=%u\n",(uint32_t)sizeof(int));
+	printf("sizeof(long)=%u\n",(uint32_t)sizeof(long));
+	printf("sizeof(long long)=%u\n",(uint32_t)sizeof(long long));
+	printf("sizeof(float)=%u\n",(uint32_t)sizeof(float));
+	printf("sizeof(double)=%u\n",(uint32_t)sizeof(double));
+	printf("sizeof(long double)=%u\n",(uint32_t)sizeof(long double));
 
 	return 0;
 }
