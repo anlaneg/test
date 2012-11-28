02818c8849ec64f491706e8e29c7f805fd126fee
anlaneg <anlaneg@a.com>
2012-11-28 09:55:20 +0800

    添加工作区创建shell(可进一步细化

---
+++ tool/mkworkspace.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#$0 指需要创建目录的父目录
+function do_make_workspace()
+{
+	parent_path=$1;
+	mkdir -p $parent_path/bin
+	mkdir -p $parent_path/document
+	mkdir -p $parent_path/mk
+	mkdir -p $parent_path/src/include
+	mkdir -p $parent_path/lib
+	mkdir -p $parent_path/tool
+	mkdir -p $parent_path/test
+}
+
+
+#在当前目录下创建工作区
+do_make_workspace `pwd`
+
