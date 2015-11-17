d6c2589280fab915efee54fa6d344652f4355221
xxat <xxat@example.com>
2015-11-17 22:58:57 +0800

    add webclick

---
+++ tool/webclick/webclick.py
@@ -0,0 +1,19 @@
+
+class WebClick(object):
+    def _get_element_by_tag_id(self,tag_id):
+        try:
+            tag = xx.get_by_id()
+        except xx as e:
+            xx.get
+        return tag
+
+    def __init__(self):
+        pass
+    def click(self,tag_id):
+        return self
+    def input(self,tag_id,value):
+        return self
+    def load(self,url):
+        return self
+    def get(self,tag_id):
+        return self
