71c655dfdfd85375919109d1045c9189af9000d4
xxat <xxat@example.com>
2015-11-29 22:07:04 +0800

    add cfg

---
+++ tool/release/release/cfg/cfg.py
@@ -0,0 +1,12 @@
+class Config(object):
+    def __init__(self):
+        self.cfg=[]
+    def addConfig(self,segment,config_name,config_type,default_value,helper,config_param):
+        pass
+    def addConfig(self,*config):
+        pass
+    def get_config_template(self):
+        pass
+    def parse_config_file(self,path):
+        pass
+    
