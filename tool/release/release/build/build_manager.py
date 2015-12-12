475ac86a8b22d7047ae55da9edaccb967c134c3a
anlaneg <anlaneg@126.com>
2015-12-12 17:59:34 +0800

    modify localhost

--- tool/release/release/build/build_manager.py
+++ tool/release/release/build/build_manager.py
@@ -5,9 +5,11 @@ class BuildManager(event.EventBase):
         super(BuildManager,self).__init__(cfg)
         self.cwd=self.get_or_raise(cfg,'workspace-dir')
         self.host=self.get_or_raise(cfg,'host')
+        if self.host == 'localhost':
+            self.host = '127.0.0.1'
         self.username=""
         self.password=""
-        if self.host != '127.0.0.1' and self.host != 'localhost':
+        if self.host != '127.0.0.1':
             self.username=self.get_or_raise(cfg,'username')
             self.password=self.get_or_raise(cfg,'password')
         self.script=self.get_or_raise(cfg,'build-script')
