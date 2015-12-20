818c3983309c24c128883025d2d1f9d003a44d33
xxat <xxat@example.com>
2015-12-20 22:22:36 +0800

    add runtime

---
+++ tool/dweb/xruntime/src/cn/along/xrt/gen/XDefaultValue.java
@@ -0,0 +1,17 @@
+package cn.along.xrt.gen;
+
+public class XDefaultValue
+{
+	public XDefaultValue(int[] array_size,String[] def_value)
+	{
+		
+	}
+	
+	public void fillByteArray()
+	public Class<?> getByteArray()
+	{
+		int[][] a = new int[2][3];
+		
+		return new int[2];
+	}
+}
