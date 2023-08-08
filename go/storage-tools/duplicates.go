dff86bb9af18933e994d287f65cdf76eb82f5c87
anlaneg <anlaneg@126.com>
2023-08-08 22:22:04 +0800

    test: If the file size is not equal, it is not a duplicate.

--- go/storage-tools/duplicates.go
+++ go/storage-tools/duplicates.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
-	//"path/filepath"
+	"path/filepath"
 )
 
 const (
@@ -20,8 +20,8 @@ const (
  */
 
 type DupOpt struct {
-	cmp_name      bool
-	cmp_size      bool
+	//cmp_name      bool
+	//cmp_size      bool
 	cmp_full_data bool
 	cmp_part_data bool
 	output_path   string
@@ -30,8 +30,8 @@ type DupOpt struct {
 
 var (
 	optGlobal = &DupOpt{
-		cmp_name:      false,
-		cmp_size:      false,
+		//cmp_name:      false,
+		//cmp_size:      false,
 		cmp_full_data: false,
 		cmp_part_data: true,
 		output_path:   c_OUTPUT_PATH,
@@ -62,13 +62,7 @@ func pathExistsError(err error, exists bool) string {
 }
 
 func argmentsParse() {
-	flag.BoolVar(&optGlobal.cmp_name, "n", false, "compare with file name")
-	flag.BoolVar(&optGlobal.cmp_name, "names", false, "compare with file name")
-
-	flag.BoolVar(&optGlobal.cmp_size, "s", false, "compare with file size")
-	flag.BoolVar(&optGlobal.cmp_size, "size", false, "compare with file size")
-
-	flag.BoolVar(&optGlobal.cmp_part_data, "d", true, "compare with file part data")
+	flag.BoolVar(&optGlobal.cmp_part_data, "p", true, "compare with file part data")
 	flag.BoolVar(&optGlobal.cmp_part_data, "part-data", true, "compare with file part data")
 
 	flag.BoolVar(&optGlobal.cmp_full_data, "f", false, "compare with file full data")
@@ -79,7 +73,14 @@ func argmentsParse() {
 
 	flag.Parse()
 
-	for _, path := range flag.Args() {
+	set := make(map[string]int,0)
+	for _, p := range flag.Args() {
+		path, err := filepath.Abs(p)
+		if err != nil {
+			fmt.Printf("ignore invalid path :%s\n", p)
+			continue 
+		}
+		
 		stat, err := os.Stat(path)
 		if err != nil {
 			fmt.Printf("os.Stat error %s\n", err.Error())
@@ -91,19 +92,19 @@ func argmentsParse() {
 			fmt.Printf("path %s error\n", path)
 			continue
 		}
-
-		fmt.Printf("add path: %s\n", path)
-		optGlobal.src_path = append(optGlobal.src_path, path)
+		
+		set[path] = 1
 	}
 
-	//TODO 文件路径去重
+	for src := range set {
+		fmt.Printf("add path: %s\n", src)
+		optGlobal.src_path = append(optGlobal.src_path, src)
+	}
 }
 
 func (opt *DupOpt) validator() bool {
-	if opt.cmp_full_data && (opt.cmp_name || opt.cmp_size || opt.cmp_part_data) {
-		fmt.Println("option -n/--names,-s/--size,-d/--part-data will be ignore")
-		opt.cmp_name = false
-		opt.cmp_size = false
+	if opt.cmp_full_data && opt.cmp_part_data {
+		fmt.Println("option -d/--part-data will be ignore")
 		opt.cmp_part_data = false
 	}
 
@@ -112,8 +113,8 @@ func (opt *DupOpt) validator() bool {
 		opt.output_path = c_OUTPUT_PATH
 	}
 
-	if !opt.cmp_full_data && !opt.cmp_name && !opt.cmp_size && !opt.cmp_part_data {
-		fmt.Printf("option -n/--names | -s/--size | -f/--full-data | -d/--part-data miss", c_OUTPUT_PATH)
+	if !opt.cmp_full_data && !opt.cmp_part_data {
+		fmt.Printf("option -f/--full-data | -d/--part-data miss", c_OUTPUT_PATH)
 		return false
 	}
 
@@ -144,43 +145,6 @@ func doFileMD5(path string) (string, error) {
 	}
 
 	return fmt.Sprintf("%x", h.Sum(nil)), nil
-}
-
-func doMD5(path string, offset int64, length int64) (string, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-
-	secReader := io.NewSectionReader(f, offset, length)
-	h := md5.New()
-	if _, err := io.Copy(h, secReader); err != nil {
-		return "", err
-	}
-
-	return fmt.Sprintf("%x", h.Sum(nil)), nil
-}
-
-func doPartFileMD5(path string, info os.FileInfo) (string, error) {
-	fileSize := info.Size()
-	const M int64 = 1 * 1024 * 1024
-	if fileSize < 1*M {
-		//1M
-		return doFileMD5(path)
-	} else if fileSize < 10*M {
-		//bigger than 1M
-		return doMD5(path, 0, 1*M)
-	} else if fileSize < 100*M {
-		//bigger than 10M
-		return doMD5(path, 5*M, 1*M)
-	} else if fileSize < 1*1024*M {
-		// bigger than 100M
-		return doMD5(path, 50*M, 1*M)
-	} else {
-		// bigger than 1G
-		return doMD5(path, 100*M, 1*M)
-	}
 }
 
 func main() {
--- go/storage-tools/file-info.go
+++ go/storage-tools/file-info.go
@@ -9,15 +9,15 @@ import (
 	"path/filepath"
 )
 
-type FileInfoCollect struct {
+type FileInfo struct {
 	size int64
 	name string
 	md5  string
 	path string
 }
 
-func collectFileInfo(opt *DupOpt) (map[string]*FileInfoCollect, error) {
-	filesInfo := make(map[string]*FileInfoCollect, 0)
+func collectFileInfo(opt *DupOpt) (map[string]*FileInfo, error) {
+	filesInfo := make(map[string]*FileInfo, 0)
 
 	for _, path := range opt.src_path {
 		path, _ = filepath.Abs(path)
@@ -34,18 +34,20 @@ func collectFileInfo(opt *DupOpt) (map[string]*FileInfoCollect, error) {
 				return nil
 			}
 
-			collect := &FileInfoCollect{
+			collect := &FileInfo{
 				name: name,
 				path: path,
 				size: info.Size(),
 			}
 
 			var e error = nil
-			if opt.cmp_part_data {
-				collect.md5, e = doPartFileMD5(path, info)
-			} else if opt.cmp_full_data {
-				collect.md5, e = doFileMD5(path)
-			}
+			/*
+				if opt.cmp_part_data {
+					collect.md5, e = doPartFileMD5(path, info)
+				} else if opt.cmp_full_data {
+					collect.md5, e = doFileMD5(path)
+				}*/
+			collect.md5, e = doPartFileMD5(path, info)
 			if e != nil {
 				return fmt.Errorf("search file %s,err=%s", path, e.Error())
 			}
@@ -62,20 +64,45 @@ func collectFileInfo(opt *DupOpt) (map[string]*FileInfoCollect, error) {
 	return filesInfo, nil
 }
 
-func duplicates(opt *DupOpt) (map[string][]*FileInfoCollect, int, error) {
+/*func duplicates(opt *DupOpt) (map[string][]*FileInfo, int, error) {
 	filesInfo, err := collectFileInfo(opt)
 	if err != nil {
 		return nil, 0, err
 	}
 
-	result := make(map[string][]*FileInfoCollect, 0)
+	result := make(map[string][]*FileInfo, 0)
 	for _, info := range filesInfo {
 		if _, dup := result[info.md5]; !dup {
-			result[info.md5] = []*FileInfoCollect{info}
+			result[info.md5] = []*FileInfo{info}
 		} else {
 			result[info.md5] = append(result[info.md5], info)
 		}
 		//fmt.Printf("name=%s,size=%d,md5=%s,path=%s\n",info.name,info.size,info.md5,k)
+	}
+
+	for key, v := range result {
+		if len(v) <= 1 {
+			delete(result, key)
+		}
+	}
+
+	return result, len(filesInfo), nil
+}*/
+
+func duplicates(opt *DupOpt) (map[string][]*FileInfo, int, error) {
+	filesInfo, err := collectFileInfo(opt)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	result := make(map[string][]*FileInfo, 0)
+	for _, info := range filesInfo {
+		result_key := fmt.Sprintf("%s-%d", info.md5, info.size)
+		if _, dup := result[result_key]; !dup {
+			result[result_key] = []*FileInfo{info}
+		} else {
+			result[result_key] = append(result[result_key], info)
+		}
 	}
 
 	for key, v := range result {
